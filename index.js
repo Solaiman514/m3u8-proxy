7539649fb6b1bca427838e13c71cb544dfc64b9c
MD. Solaiman Islam <71056248+Solaiman514@users.noreply.github.com>
2025-06-23 09:41:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const request = require("request");
+const express = require('express');
+const request = require('request');
 const app = express();
 
-app.use(cors());
+app.get('/aajtak', (req, res) => {
+  const url = "https://z5ak-cmaflive.zee5.com/cmaf/live/2105503/AAJTAKHDNEWSELE/index-connected.m3u8?hdntl=exp=1750731717~acl=%2f*~data=hdntl~hmac=e716a58095d38ccf29ffa4245a1fae16148e78325c21f322f5978c68b240cf56";
 
-app.get("/proxy", (req, res) => {
-  const url = req.query.url;
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 ygx/69.1 Safari/537.36'
+  };
 
-  if (!url) {
-    return res.status(400).send("Missing 'url' parameter");
-  }
-
-  req.pipe(request(url)).pipe(res);
+  request({ url, headers }).pipe(res);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Proxy server running at http://localhost:3000/aajtak');
 });
